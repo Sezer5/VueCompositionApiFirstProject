f5883f631c77a3d2bfbebe1449c4afcde784d554
Sezer5 <unalmissezer@gmail.com>
2025-11-16 22:28:54 +0300

    Form Tasarımı Css İşlemleri Bitti

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,7 @@
   <div>
     <app-header/>
       <div class="container">
-          <!-- <Person :name="data.name" :lastName="data.lastName" :personAge="data.age" @update-lastname="updateLastname"/> -->
-          <!-- <button @click="updateName">Adı Güncelle</button>
-          <h1>{{name}}</h1> -->
-          <!-- <Students /> -->
-         <!-- <Studentscore>
-              <h3>Slot Dersi</h3>
-              <template v-slot:name>
-                
-                <ul>
-                    <li v-for="student in students" :key="student.id">
-                        <span>{{student.name}} :</span><span>{{student.score}}</span>
-                    </li>
-                </ul>
-              </template>
-              <template v-slot:example>
-                <h2>Deneme Slotu</h2>
-              </template>
-         </Studentscore>
-         <button @click="activeComp=StudentOne">Öğrenci 1</button>
-         <button @click="activeComp=StudentTwo">Öğrenci 2</button>
-         <!-- <StudentOne v-if="activeComp === StudentOne" />
-         <StudentTwo v-if="activeComp === StudentTwo" /> -->
-         <!--<component :is="activeComp"></component> -->
-         <Lifecycle v-if="showComponent"/>
+          <ContactForm />
       </div>
     <Footer />
   </div>
@@ -36,53 +13,8 @@
 </style>
 <script setup>
 import Footer from '@/components/Fixed/Footer.vue'
-import Person from '@/components/UserInformation/Person.vue'
-import Students from '@/components/Students/Students.vue'
-import Studentscore from '@/components/Students/Studentscore.vue'
-import StudentOne from '@/components/StudentsMath/StudentOne.vue'
-import StudentTwo from '@/components/StudentsMath/StudentTwo.vue'
-import Lifecycle from '@/components/Cycle/Lifecycle.vue'
-import {ref,shallowRef} from 'vue'
-const showComponent = ref(true)
+import ContactForm from '@/components/Contact/ContactForm.vue'
 
-setTimeout(()=>{
-    showComponent.value=false;
-},4000)
-const activeComp = shallowRef(StudentOne)
-    // export default{
-    //     setup(){
-    //       const name='Sezer Ünalmış';
-    //       return {name};
-    //     }
-
-    // }
-    import {reactive,provide} from 'vue'
-    const students=reactive([{
-      id:1,
-      name:'Sezer',
-      score:65
-    },
-    {
-      id:2,
-      name:'Seda',
-      score:75
-    },
-    {
-      id:3,
-      name:'Cahit',
-      score:85
-    }]);
-    const name=ref('Sezer');
-    const updateName = ()=>{
-      data.name='Seda';
-    }
-    const updateLastname = (lastName)=>{
-      data.lastName=lastName;
-    }
-    const updateStudent = ()=>{
-      students[0].score=70;
-    }
-    provide('students',{students,updateStudent})
 </script>
 <style>
     *{
---
+++ src/components/Contact/ContactForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <form>
+        <div class="row">
+            <h1>İletişim</h1>
+            <hr>
+            <div class="mb-3">
+                <label>Ad</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label>Soyad</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <h4>Cinsiyet</h4>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input">
+                    <label for="">Kadın</label>
+                </div>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input">
+                    <label for="">Erkek</label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label>Konu</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <h4>Hangi Amaçla Yazıyorsunuz?</h4>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input">
+                    <label for="form-check-label">Canlı Eğitimler</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input">
+                    <label for="form-check-label">Video Eğitimler</label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label>Mesajınız</label>
+                <textarea class="form-control" id="" cols="30" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary">Gönder</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
