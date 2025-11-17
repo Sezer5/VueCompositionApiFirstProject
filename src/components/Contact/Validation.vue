c5c02b7a8482fff444550d6abef4a5a803dabe3b
Sezer5 <unalmissezer@gmail.com>
2025-11-17 23:39:53 +0300

    Yup en kısa ve kullanılan mantık

--- src/components/Contact/Validation.vue
+++ src/components/Contact/Validation.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <Form>
+        <Form :validation-schema="formSchema">
             <div class="mb-3">
                 <label>Ad:</label>
-                <Field :rules=[isRequired,validateName] name="name" class="form-control" placeholder="Adınızı Giriniz"/>
+                <Field  name="name" class="form-control" placeholder="Adınızı Giriniz"/>
                 <ErrorMessage name="name" as="div" v-slot="{message}">
                     <div class="alert alert-danger"> 
                         {{message}}
@@ -17,20 +17,25 @@
 
 <script setup>
 import { Form, Field , ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
 
-const isRequired = (value)=>{
-    if(!value){
-        return 'Bu alan zorunludur!'
-    }
-    return true;
-}
+// const isRequired = (value)=>{
+//     if(!value){
+//         return 'Bu alan zorunludur!'
+//     }
+//     return true;
+// }
 
-const validateName = (value)=>{
-    if(value==='osman'){
-        return 'Bu isim girilemez!'
-    }
-    return true;
-}
+// const validateName = (value)=>{
+//     if(value==='osman'){
+//         return 'Bu isim girilemez!'
+//     }
+//     return true;
+// }
+
+const formSchema = yup.object({
+    name:yup.string().required('Bu alan zorunludur').min(6,'Lütfen minimum 6 karakter giriniz!')
+})
 
 </script>
 
