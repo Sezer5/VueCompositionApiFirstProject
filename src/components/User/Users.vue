0b84537cf6d4765a4fc7b8e6bca1de9ee3cf9cc7
Sezer5 <unalmissezer@gmail.com>
2025-11-18 21:59:45 +0300

    json db ve axios ile veri çekme ve veri oluşturma işlemi bitti önemli

--- src/components/User/Users.vue
+++ src/components/User/Users.vue
@@ -1,9 +1,34 @@
 <template>
-    Users
+    <div class="row">
+        <div class="col-md-3 mb-4" v-for="user in data.users" :key="user.id"> 
+                <div class="card" style="width: 18rem;">
+                    <img :src="'http://placebeard.it/300/300/?'+user.id" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{user.name}} {{user.lastname}}</h5>                        
+                    </div>
+                </div>
+        </div>
+
+    </div>
 </template>
 
 <script setup>
-
+import axios from 'axios';
+import {onMounted,reactive} from 'vue'
+const data=reactive({
+    users:[],
+})
+    const getUser=async()=> {
+        try {
+            const response = await axios.get('http://localhost:3000/users');
+            data.users = response.data;
+        } catch (error) {
+            console.error(error);
+        }
+        }
+onMounted(()=>{
+    getUser()
+})
 </script>
 
 <style>
